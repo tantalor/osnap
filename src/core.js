de64af28d1b6bcc7d754bc4e90b89dccd68daa57
Brad Fults <bfults@gmail.com>
2008-02-29 16:11:35 -0800

    Add core source file.

---
+++ src/core.js
@@ -0,0 +1,24 @@
+/*
+Script: core.js
+        Contains the core functionality for Osnap.
+
+License:
+        MIT-style license.
+*/
+
+/*
+Class: Osnap
+        The base class for the Osnap library, containing the bootstrap and
+        initialization code.
+
+Arguments:
+        sRootId - string; HTML id of the root element of the Osnap application
+        urlStart - string; URL to which Osnap will make its first call and
+        derive the base URL for future calls from
+*/
+
+var Osnap = new Class({
+    initialize: function (sRootId, urlStart) {
+        // TODO: implement
+    }
+});
